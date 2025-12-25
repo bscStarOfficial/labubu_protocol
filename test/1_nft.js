9e5433b8454f1aa9ac021cf2065585d2012a46d9
jc <pandangoup@outlook.com>
2025-12-25 11:22:47 +0800

    test

---
+++ test/1_nft.js
@@ -0,0 +1,51 @@
+const {expect} = require("chai");
+const {ethers, deployments} = require("hardhat");
+const common = require("./util/common");
+const {loadFixture, time} = require("@nomicfoundation/hardhat-network-helpers");
+const {nftInit, sendTransaction, balanceOf, totalSupply, safeMint, setMaxTokenId} = require("./util/nft");
+const {parseEther} = require("ethers/lib/utils");
+
+let deployer, reserve, A, B, C, D, E, F, G;
+let manager, nft;
+
+async function initialFixture() {
+  await deployments.fixture();
+  await nftInit();
+
+  [nft, manager] = await common.getContractByNames(["LabubuNFT", 'Manager']);
+  [deployer, reserve, A, B, C, D, E, F, G] = await common.getAccounts(
+    ["deployer", "reserve", "A", "B", "C", "D", "E", "F", "G"]
+  );
+}
+
+describe("NFT购买", function () {
+  before(async () => {
+    await initialFixture();
+    await nft.setOnlyAA(false);
+  })
+  it('转账bnb直接购买，钱转到合约地址', async function () {
+    await expect(safeMint(A)).to.changeEtherBalance(
+      reserve, parseEther("0.6")
+    )
+    expect(await balanceOf(A)).to.eq(1)
+    expect(await totalSupply()).to.eq(1)
+  });
+  it("一个地址只能买一个", async function () {
+    await expect(safeMint(A)).to.revertedWith("one")
+  })
+  it("截止批次后，无法购买", async function () {
+    await setMaxTokenId(2)
+    await expect(safeMint(B)).to.revertedWith("!max")
+  })
+  it("金额不对", async function () {
+    await expect(safeMint(A, 0.59)).to.revertedWith("!price")
+  })
+})
+
+describe("NFT分红", function () {
+  before(async () => {
+    await initialFixture();
+  })
+})
+
+// TODO 测试合约升级
---
+++ test/util/nft.js
@@ -0,0 +1,61 @@
+const {parseEther, formatEther, parseUnits} = require("ethers/lib/utils");
+const {ethers} = require("hardhat");
+const common = require("./common");
+const {setBalance} = require("@nomicfoundation/hardhat-network-helpers");
+const {BigNumber} = require("bignumber.js");
+
+let nft;
+
+async function nftInit() {
+  [nft] = await common.getContractByNames(["LabubuNFT"]);
+}
+
+function sendTransaction(account) {
+  return account.sendTransaction({
+    from: account,
+    to: nft.address,
+    value: parseEther('0.6')
+  });
+}
+
+function safeMint(account, value = 0.6) {
+  return nft.connect(account).safeMint(account.address, {
+    value: parseEther(value.toString()),
+  });
+}
+
+function claim(account) {
+  return nft.claim(account.address);
+}
+
+function sendReward(amount) {
+  return nft.sendReward({value: parseEther(amount.toString())});
+}
+
+function setMaxTokenId(id) {
+  return nft.setMaxTokenId(id);
+}
+
+async function maxTokenId() {
+  return await nft.maxTokenId()
+}
+
+async function totalSupply() {
+  return await nft.totalSupply()
+}
+
+async function balanceOf(account) {
+  return await nft.balanceOf(account.address)
+}
+
+module.exports = {
+  nftInit,
+  sendTransaction,
+  safeMint,
+  claim,
+  sendReward,
+  setMaxTokenId,
+  maxTokenId,
+  totalSupply,
+  balanceOf
+}
